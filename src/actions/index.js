aa2f9c1f9f92cd2a2268ff6868b604f935a10087
Fatima Naveed <fnaveed@fnaveed013294.starbucks.net>
2018-09-19 09:40:47 -0700

    fixed error

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import elasticsearch from 'elasticsearch';
 import _ from 'lodash';
 import moment from 'moment';
-import VOLUME_DATA from 'volumeData';
+
 
 import { getWordCloudDataRequest, getVolumeData} from '../api/requests';
 
@@ -86,7 +86,68 @@ export const fetchTotalVolumeData = (startDate, endDate, company) => {
         console.log(timeSpanData['endDate'])
         console.log("DATES IN BETWEEN")
         console.log(timeSpanData['breakouts'])
-        timeSpanData['data'] = VOLUME_DATA
+        timeSpanData['data'] = [
+          {
+            'company': 'Starbucks',
+            'values': [
+              {'date': new Date(2018, 8, 16), 'volume': 10},
+              {'date': new Date(2018, 8, 17), 'volume': 20},
+              {'date': new Date(2018, 8, 18), 'volume': 5},
+              {'date': new Date(2018, 8, 19), 'volume': 8},
+              {'date': new Date(2018, 8, 20), 'volume': 30},
+              {'date': new Date(2018, 8, 21), 'volume': 32},
+              {'date': new Date(2018, 8, 22), 'volume': 25},
+              {'date': new Date(2018, 8, 23), 'volume': 20},
+              {'date': new Date(2018, 8, 24), 'volume': 12},
+              {'date': new Date(2018, 8, 25), 'volume': 10}
+            ],
+            'dots': [
+              {'company': 'Starbucks', 'date': new Date(2018, 8, 16), 'volume': 20, 'sentiment': 'green'},
+              {'company': 'Starbucks', 'date': new Date(2018, 8, 21), 'volume': 32, 'sentiment': 'green'},
+              {'company': 'Starbucks', 'date': new Date(2018, 8, 22), 'volume': 25, 'sentiment': 'red'}
+            ]
+          },
+          {
+            'company': "Dutch Bros",
+            'values': [
+              {'date': new Date(2018, 8, 16), 'volume': 20},
+              {'date': new Date(2018, 8, 17), 'volume': 10},
+              {'date': new Date(2018, 8, 18), 'volume': 15},
+              {'date': new Date(2018, 8, 19), 'volume': 18},
+              {'date': new Date(2018, 8, 20), 'volume': 10},
+              {'date': new Date(2018, 8, 21), 'volume': 12},
+              {'date': new Date(2018, 8, 22), 'volume': 35},
+              {'date': new Date(2018, 8, 23), 'volume': 10},
+              {'date': new Date(2018, 8, 24), 'volume': 12},
+              {'date': new Date(2018, 8, 25), 'volume': 14}
+            ],
+            'dots': [
+              {'company': "Dutch Bros", 'date': new Date(2018, 8, 19), 'volume': 18, 'sentiment': 'red'},
+              {'company': "Dutch Bros", 'date': new Date(2018, 8, 25), 'volume': 14, 'sentiment': 'red'},
+            ]
+          },
+          {
+            'company': 'Dunkin Donuts',
+            'values': [
+              {'date': new Date(2018, 8, 16), 'volume': 20},
+              {'date': new Date(2018, 8, 17), 'volume': 30},
+              {'date': new Date(2018, 8, 18), 'volume': 25},
+              {'date': new Date(2018, 8, 19), 'volume': 8},
+              {'date': new Date(2018, 8, 20), 'volume': 19},
+              {'date': new Date(2018, 8, 21), 'volume': 14},
+              {'date': new Date(2018, 8, 22), 'volume': 25},
+              {'date': new Date(2018, 8, 23), 'volume': 20},
+              {'date': new Date(2018, 8, 24), 'volume': 19},
+              {'date': new Date(2018, 8, 25), 'volume': 12}
+            ],
+            'dots': [
+              {'company': 'Dunkin Donuts', 'date': new Date(2018, 8, 17), 'volume': 30, 'sentiment': 'red'},
+              {'company': 'Dunkin Donuts', 'date': new Date(2018, 8, 20), 'volume': 19, 'sentiment': 'gray'},
+              {'company': 'Dunkin Donuts', 'date': new Date(2018, 8, 21), 'volume': 14, 'sentiment': 'green'},
+              {'company': 'Dunkin Donuts', 'date': new Date(2018, 8, 24), 'volume': 19, 'sentiment': 'red'},
+            ]
+          }
+        ]
         dispatch(receiveTotalVolumeData(timeSpanData));
     }
 }
